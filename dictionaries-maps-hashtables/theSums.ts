3f9f127b2e09a176151f999c0cd0920d963ec94d
Kayla <killiankaylad@gmail.com>
2022-06-14 11:20:08 -0400

    addition: completed threeNumberSum. Played around with optimizing the LRU

--- dictionaries-maps-hashtables/theSums.ts
+++ dictionaries-maps-hashtables/theSums.ts
@@ -33,12 +33,14 @@ function twoNumberSum(array: number[], sum: number) {
 
 // Pointers - O(nlogn) time | O(n) space
 function twoNumberSumP(array: number[], targetSum: number) {
+  // O(nlogn)
   array.sort((a,b) => a - b);
   let left: number = 0;
   let right: number = array.length - 1;
 
   // until the pointers meet. Compare their sum to targetSum
   // move a pointer depending on wether its greater/less than targetSum
+  // O(n)
   while (left < right) {
     let currentSum = array[left] + array[right];
     if (currentSum === targetSum) {
@@ -52,3 +54,50 @@ function twoNumberSumP(array: number[], targetSum: number) {
   return [];
 }
 // console.log(twoNumberSumP([1,2,3,4], 4));
+
+/** Three Number Sum
+ * Output: all triplets in a two-dimensional array. The numbers in each triplet should be assorted in ascending order, and the triplets ascending as well.
+ * Input:
+ * "array": [12, 3, 1, 2, -6, 5, -8, 6],
+ * "targetSum": 0
+ * Output:
+ * [[-8, 2, 6], [-8, 3, 5], [-6, 1 5]]
+ */
+/**
+ * Sort the array. nlogn
+ * Use three pointers: an index, left, and right.
+ * check the sum of the pointers
+ * if theres a match, push to an output array, and update left and right pointers
+ * if not move the left or right pointer depending on value of currentSum
+ * O(n^2)
+ */
+type Triplet = [number, number, number];
+function threeNumberSum(array: number[], targetSum: number) {
+  array.sort((a, b) => a - b);
+  const outputArray: Triplet[] = [];
+
+  for (let index = 0; index < array.length -2; index++) {
+    let left = index + 1;
+    let right = array.length - 1;
+
+    while (left < right) {
+      const currentSum = array[index] + array[left] + array[right];
+      if (currentSum === targetSum) {
+        outputArray.push([array[index], array[left], array[right]]);
+        // move both pointers
+        left++;
+        right--;
+      } else if (currentSum > targetSum) {
+        right--;
+      } else if (currentSum < targetSum) {
+        left++;
+      }
+    }
+  }
+  return outputArray;
+}
+//console.log(threeNumberSum([12, 3, 1, 2, -6, 5, -8, 6], 0));
+
+/** Four Number Sum
+ *
+ */
--- maps-linkedlists/LRU-refactored.js
+++ maps-linkedlists/LRU-refactored.js
@@ -1,16 +1,23 @@
 /*
+LRU Cache - Least recently used Cache
+When the Cache is full, remove the least recently used. Accessing or adding a key in the cache
+updates it to the most recently used.
+
 NewCache(size) - creates new lru cache
-getItem - Get(key) - gets an item in the cache
-setHead - Put(key, value) - puts an item into the cache
+Get(key) - gets an item in the cache
+Put(key, value) - puts an item into the cache
 Delete(key) - deletes an item in the cache
 
 cache = NewCache(2)
 cache.put("hello", "world")
 cache.put("hello2", "world")
+  hello2 <-> hello
 cache.get("hello")
+  hello <-> hello2
 cache.put("hello3", "world")
-cache.get("hello") = nil
+  hello3 <-> hello <-> hello2
 
+adding a duplicate key just updates its value.
 */
 class ListNode {
   constructor(key, value) {
@@ -28,56 +35,18 @@ class Cache {
     this.map = new Map();
   }
 
+  // Put - puts an item into the cache
+
+  // Get - gets an item in the cache
+
+  // Delete - deletes an item in the cache
+
   // check size
   checkSize() {
     return this.map.size >= this.maxSize;
   }
   checkKey(key) {
     return this.map.has(key);
-  }
-
-  // set head - Puts an new item into the LRU or
-  // updates the head when an item is accessed
-  setHead(key, value) {
-    if (key === null) {
-      console.log('key is null');
-      return;
-    }
-    // The LRU doesn't have the key: It's empty or adding a new entry
-    if (!this.checkKey(key)) {
-      // No elements in the cache
-      const newNode = new ListNode(key, value);
-      if (this.head === null) {
-        this.head = newNode, this.tail = newNode;
-        return;
-      }
-      // New Entry - set it as head, update old bindings
-      const oldHead = this.head;
-      newNode.prev = oldHead;
-      this.head = newNode;
-      return;
-    }
-    // The LRU Has the key, so we are rewriting a duplicate or moving an old entry
-
-
-  }
-
-  // get item
-  getItem(key) {}
-
-  // update list links
-  updateLinks() {
-
-  }
-
-    // removeItem
-  removeItem() {
-
-  }
-
-  // remove tail
-  removeTail() {
-
   }
 }
 
